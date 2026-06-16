1354834c1f45d83c4eb70353f6c9df9aeca2d038
Yevestevez <yevestevez.cg@gmail.com>
2026-06-16 13:44:00 +0200

    Add testing dependencies

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+    testDir: './tests/e2e',
+    webServer: {
+        command: 'pnpm build && pnpm preview',
+        url: 'http://localhost:4321',
+        reuseExistingServer: !process.env.CI,
+    },
+    use: {
+        baseURL: 'http://localhost:4321',
+    },
+    projects: [
+        { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+        { name: 'mobile', use: { ...devices['Pixel 5'] } },
+    ],
+});
